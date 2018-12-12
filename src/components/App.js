284f91c52a4d16b294877e9bee8ed47459ae9b86
Robert Schulte <schulte.robertw@outlook.com>
2018-12-12 14:39:09 -0600

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,47 @@
 import React from 'react'
-
 import Filters from './Filters'
 import PetBrowser from './PetBrowser'
 
 class App extends React.Component {
   constructor() {
-    super()
+    super();
 
     this.state = {
       pets: [],
       filters: {
-        type: 'all'
+        type: "all"
       }
+    };
+  }
+
+  updateFilters = newFilters => {
+    this.setState({ filters: { type: newFilters } });
+    // Below does the same as above but using callback
+    // this.setState(state => {
+    //   state.filters.type = newFilters
+    //   return state
+    // })
+  };
+
+  fetchPets = () => {
+    let type = this.state.filters.type;
+    let url = "/api/pets";
+    if (type !== "all") {
+      url += `?type=${type}`;
     }
+    fetch(url)
+      .then(resp => resp.json())
+      .then(pets => {
+        this.setState({ pets: pets });
+      });
+  };
+
+  adoptPet = (petId) => {
+    this.setState(state => {
+      let pet = state.pets.find( pet => {return pet.id === petId})
+      pet.isAdopted = true
+      return state
+    })
   }
 
   render() {
@@ -24,15 +53,18 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters
+                onChangeType={this.updateFilters}
+                onFindPetsClick={this.fetchPets}
+              />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.adoptPet} />
             </div>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
